64a38b4d30e4db50731b4dc6392ddac1f02c1ee8
smenesesd <smenesesd@eafit.edu.co>
2023-02-22 10:11:32 -0500

    Actualizacion de codigo de tutorial 1 a tutorial2

--- tutorial1/taller1Laravel/taller1/routes/web.php
+++ tutorial1/taller1Laravel/taller1/routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 
 Route::get('/about', function () {
